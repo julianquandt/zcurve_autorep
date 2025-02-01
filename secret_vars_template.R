28d921ca6bfe908fa885d5083c85158bf2550931
julianquandt <julian_quandt@live.de>
2025-02-01 22:10:27 +0100

    add template for secret vars and project description

---
+++ secret_vars_template.R
@@ -0,0 +1,9 @@
+scopus_key <- "Your scopus api key here" # required for 01a_fetch_metadata_from_scopus.Rmd
+opencitations_key <- "your opencitations api key here" # required for fetching citation data from open citations
+my_gmail <- "your gmail" # required if you want to save your data in a google sheet
+google_sheet_id <- "google sheet id here" # required if you want to save your data in a google sheet; will be generated once you save something there from the script.
+google_sheet_url <- " " # no longer required
+oa_email_key <- "any of your emails here" # any e-mail so the people on Unpaywall know who is using their data (no need to register or anything)
+semantic_scholar_key <- "your semantic scholar key here" # required for fetching citation data from semantic scholar
+openai_key <- "" # required if you use the openai API to analyze document texts
+
